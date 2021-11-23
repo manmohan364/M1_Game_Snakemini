beb22cbcbab5e7afc314e9137ffc0ab83ea83e0e
Man Mohan <94183432+manmohan364@users.noreply.github.com>
2021-11-23 22:17:31 +0530

    Add files via upload

---
+++ 3_Implementation/inc/sanke.h
@@ -0,0 +1,11 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <windows.h>
+
+int setup(int x,int y,int fruitx,int fruity, int gameover,int score,int height, int width);
+int input(int flaf,int gameover);
+int logic(int flag,int y,int x,int height,int width, int gameover,int fruitx,int fruity,int score);
+
