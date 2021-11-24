fd7c6d9d7c1af19ae51cbef01e6bacb854ac0b7f
Man Mohan <94183432+manmohan364@users.noreply.github.com>
2021-11-24 13:40:26 +0530

    Update sanke.h

--- 3_Implementation/inc/sanke.h
+++ 3_Implementation/inc/sanke.h
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
-
 int setup(int x,int y,int fruitx,int fruity, int gameover,int score,int height, int width);
 int input(int flaf,int gameover);
 int logic(int flag,int y,int x,int height,int width, int gameover,int fruitx,int fruity,int score);
